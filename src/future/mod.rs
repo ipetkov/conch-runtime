164d96b5743ef5feb6089ef77a01ca1dcd696d5c
Ivan Petkov <ivanppetkov@gmail.com>
2017-01-08 13:10:32 -0800

    Add `Fuse` adapter that behaves similarly to futures::Fuse
    * `Fuse` will ignore any future calls to `poll` or `cancel` after the
    future has been resolved or cancelled (in the case of `poll`,
    `NotReady` is always returned, similar to the `futures` version)
    * Also updated the `EnvFuture::cancel` contract to explicitly note that
    cancelling a future that has never been polled is a valid use case
    * At first it seemed like this guarantee would not be needed since
    `Fuse` could handle the use case for us, but unfortunately, we don't
    know if the future was polled before we wrap it (hence no guarantees
    can be made by us)

--- src/future/mod.rs
+++ src/future/mod.rs
@@ -2,10 +2,12 @@
 //! execution with futures.
 
 mod invert;
+mod fuse;
 mod pinned;
 
 pub use futures::{Async, Poll};
 pub use self::invert::*;
+pub use self::fuse::Fuse;
 pub use self::pinned::*;
 
 /// A trait for objects that behave exactly like the `Future` trait from the
@@ -37,6 +39,10 @@ pub trait EnvFuture<E: ?Sized> {
     /// forever, or otherwise cause wrong behavior. The `EnvFuture` trait itself
     /// provides no guarantees about the behavior of `poll` after a future has completed.
     ///
+    /// Callers who may call `poll` too many times may want to consider using
+    /// the `fuse` adaptor which defines the behavior of `poll`, but comes with
+    /// a little bit of extra cost.
+    ///
     /// Additionally, calls to `poll` must always be made from within the
     /// context of a task. If a current task is not set then this method will
     /// likely panic.
@@ -51,7 +57,9 @@ pub trait EnvFuture<E: ?Sized> {
     /// it wishes to undo (e.g. temporarily overriding a file descriptor).
     ///
     /// Thus if a caller cares about returning the environment to a valid state,
-    /// they must call `cancel` before dropping the future.
+    /// they must call `cancel` before dropping the future. Cancelling a future
+    /// which has never been polled is considered a valid use case, and should
+    /// be supported by the implementation.
     ///
     /// Caller should take care to cancel this future with the same environment
     /// that was provided to `poll`. An implementation may panic or yield
@@ -60,16 +68,37 @@ pub trait EnvFuture<E: ?Sized> {
     /// # Panics
     ///
     /// If a future has completed (returned `Ready` or `Err` from `poll`, or
-    /// `cancel` invoked) or never been `poll`ed before, then any future calls
-    /// to `cancel` may panic, block forever, or otherwise cause wrong behavior.
-    /// The `EnvFuture` trait itself provides no guarantees about the behavior
-    /// of `cancel` after a future has completed or if it has never been `poll`ed.
+    /// `cancel` invoked) then any future calls to `cancel` may panic, block
+    /// forever, or otherwise cause wrong behavior. The `EnvFuture` trait itself
+    /// provides no guarantees about the behavior of `cancel` after a future has
+    /// completed.
+    ///
+    /// Callers who may call `cancel` too many times may want to consider using
+    /// the `fuse` adaptor which defines the behavior of `cancel`, but comes
+    /// with a little bit of extra cost.
     fn cancel(&mut self, env: &mut E);
 
     /// Pin an environment to this future, allowing the resulting future to be
     /// polled from anywhere without needing the caller to specify an environment.
     fn pin_env(self, env: E) -> Pinned<E, Self> where E: Sized, Self: Sized {
         Pinned::new(env, self)
+    }
+
+    /// Fuse a future such that `poll` and `cancel` will never again be called
+    /// once it has completed.
+    ///
+    /// Currently once a future has returned `Ready` or `Err` from
+    /// `poll` or if `cancel` was invoked, any further calls to either method
+    /// could exhibit bad behavior such as blocking forever, panicking, never
+    /// returning, etc. If it is known that `poll` or `cancel` may be called too
+    /// often then this method can be used to ensure that it has defined semantics.
+    ///
+    /// Once a future has been `fuse`d and it returns a completion from `poll`
+    /// or cancelled via `cancel, then it will forever return `NotReady` from
+    /// `poll` (never resolve) and will ignore future calls to `cancel`. This,
+    /// unlike the trait's `poll` and `cancel` methods, is guaranteed.
+    fn fuse(self) -> Fuse<Self> where Self: Sized {
+        fuse::new(self)
     }
 }
 
