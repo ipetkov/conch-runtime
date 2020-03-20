7987ac397df17a4a3202824acc170631e7605144
Ivan Petkov <ivanppetkov@gmail.com>
2020-03-19 19:03:27 -0700

    Bring back conversion between ast::RedirectOrEnvVar to our type

--- src/eval/ast_impl.rs
+++ src/eval/ast_impl.rs
@@ -1,8 +1,7 @@
 //! This module defines various `WordEval` implementations on AST types defined by
 //! the `conch-parser` crate.
 
-use crate::eval::RedirectOrCmdWord;
-// use crate::eval::{RedirectOrCmdWord, RedirectOrVarAssig};
+use crate::eval::{RedirectOrCmdWord, RedirectOrVarAssig};
 use conch_parser::ast;
 
 mod arith;
@@ -13,8 +12,6 @@ mod redirect;
 mod simple_word;
 mod word;
 
-// pub use self::param_subst::ParameterSubstitution;
-
 impl<R, W> From<ast::RedirectOrCmdWord<R, W>> for RedirectOrCmdWord<R, W> {
     fn from(from: ast::RedirectOrCmdWord<R, W>) -> Self {
         match from {
@@ -24,11 +21,11 @@ impl<R, W> From<ast::RedirectOrCmdWord<R, W>> for RedirectOrCmdWord<R, W> {
     }
 }
 
-// impl<R, V, W> From<ast::RedirectOrEnvVar<R, V, W>> for RedirectOrVarAssig<R, V, W> {
-//     fn from(from: ast::RedirectOrEnvVar<R, V, W>) -> Self {
-//         match from {
-//             ast::RedirectOrEnvVar::Redirect(r) => RedirectOrVarAssig::Redirect(r),
-//             ast::RedirectOrEnvVar::EnvVar(k, v) => RedirectOrVarAssig::VarAssig(k, v),
-//         }
-//     }
-// }
+impl<R, V, W> From<ast::RedirectOrEnvVar<R, V, W>> for RedirectOrVarAssig<R, V, W> {
+    fn from(from: ast::RedirectOrEnvVar<R, V, W>) -> Self {
+        match from {
+            ast::RedirectOrEnvVar::Redirect(r) => RedirectOrVarAssig::Redirect(r),
+            ast::RedirectOrEnvVar::EnvVar(k, v) => RedirectOrVarAssig::VarAssig(k, v),
+        }
+    }
+}
